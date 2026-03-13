2a029744f5cd2e1914b9c762bcb0222f43c34930
javier <javier.morabaiz@gmail.com>
2026-03-13 16:23:33 -0300

    interface of UserRepository added with email verification

--- backend/roomhotel/src/main/java/com/roomhotel/roomhotel/repository/UserRepository.java
+++ backend/roomhotel/src/main/java/com/roomhotel/roomhotel/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.roomhotel.roomhotel.repository;
 
-public class UserRepository {
+import com.roomhotel.roomhotel.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
